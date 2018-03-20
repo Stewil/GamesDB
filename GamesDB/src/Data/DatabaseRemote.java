7a1ca967a2c7b59eb3182360ff5a81e4a82dbe15
Stewil <32218463+Stewil@users.noreply.github.com>
2018-03-20 16:35:14 +0100

    added login for database postgresql

--- GamesDB/src/Data/DatabaseRemote.java
+++ GamesDB/src/Data/DatabaseRemote.java
@@ -10,5 +10,8 @@ package Data;
  * @author Stefan
  */
 public class DatabaseRemote {
+    private String url = "postgres://dmztduup:bH3sPoAsfFDfBqJqeZOztV4wAEhui8X2@horton.elephantsql.com:5432/dmztduup";
+    private String user = "dmztduup";
+    private String password = "bH3sPoAsfFDfBqJqeZOztV4wAEhui8X2";
     
 }
